b1c6291a10e7489aedf88b880a4e48e49a702394
Ali Crump <alicrump@dhcp-10-250-73-237.harvard.edu>
2019-12-07 22:43:16 -0500

    added comments

--- map_prep.R
+++ map_prep.R
@@ -1,6 +1,12 @@
 library(RCurl)
 library(rjson)
 
+# I got all of this code from http://rstudio-pubs-static.s3.amazonaws.com/257443_6639015f2f144de7af35ce4615902dfd.html
+# This code is downloading the locations of the stadiums from a github account
+# then they convert from JSON to a data frame arenas which we'll use in app.R
+# for our map
+
+
 rawjson <- getURL('https://raw.githubusercontent.com/nhlscorebot/arenas/master/teams.json')
 locations <- fromJSON(rawjson)
 
@@ -13,5 +19,6 @@ arenas$lat<-as.numeric(arenas$lat)
 arenas$lng<-as.numeric(arenas$lng)
 arenas$label<-paste0(arenas$team, ' - ', arenas$name)
 
+# Move arenas to my shiny folder so my shiny app can "see" it
 
 write_rds(arenas, "nhl/arenas.rds")
